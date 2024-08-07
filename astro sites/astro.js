bb003c68bae681488c682e637d980de9dcce6074
eva13 <evabalabantaray@gmail.com>
2024-08-07 22:06:04 +0530

    modified pictures

--- astro sites/astro.js
+++ astro sites/astro.js
@@ -46,13 +46,13 @@
     });
 });
 */
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev')
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
 
 next.addEventListener('click', function(){
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide').appendChild(items[0])
-})
+});
 
 prev.addEventListener('click', function(){
     let items = document.querySelectorAll('.item')
