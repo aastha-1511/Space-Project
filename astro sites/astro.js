c3ec88a5760781293f5e9f3e4d217f9d820c1325
eva13 <evabalabantaray@gmail.com>
2024-08-07 20:11:43 +0530

    removed video

--- astro sites/astro.js
+++ astro sites/astro.js
@@ -1,5 +1,5 @@
 
-document.addEventListener('DOMContentLoaded', () => {
+/*document.addEventListener('DOMContentLoaded', () => {
     // Preload images
     const gifS = [
         'media/vid1.mp4',
@@ -45,3 +45,16 @@ document.addEventListener('DOMContentLoaded', () => {
         slide.prepend(items[items.length - 1]); // Move the last item to the beginning
     });
 });
+*/
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
